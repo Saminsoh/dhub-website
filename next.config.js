332b4ef357bb7d6be7778d99c02c386144941b08
Saminsoh <mohammed.a.s.alhosni@gmail.com>
2025-09-24 13:30:04 +0400

    Update next.config.js
    next-theme and sdk 24092025

--- next.config.js
+++ next.config.js
@@ -1,26 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  images: { 
-    unoptimized: false,
-    domains: ['localhost'],
-  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
         ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
+        '@react-native-async-storage/async-storage': false,
+      }
     }
-    return config;
+    return config
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
